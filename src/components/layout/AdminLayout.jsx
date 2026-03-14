29ff02cf597f44695c31ad8b59cd968fa886929e
Robert Dyson <rdyson2622@gmail.com>
2026-03-14 19:46:55 +0000

    File changes

--- src/components/layout/AdminLayout.jsx
+++ src/components/layout/AdminLayout.jsx
@@ -6,7 +6,7 @@ export default function AdminLayout() {
   return (
     <div className="flex min-h-screen" style={{ background: '#A9A9A9' }}>
       <AdminSidebar />
-      <main className="flex-1 overflow-auto">
+      <main className="flex-1 overflow-auto" style={{ background: '#A9A9A9' }}>
         <Outlet />
       </main>
     </div>
