bf8a1f129304960cacf88d672e75ddf4ba0f71d9
Robert Dyson <rdyson2622@gmail.com>
2026-03-14 20:42:17 +0000

    File changes

--- src/pages/Explainers.jsx
+++ src/pages/Explainers.jsx
@@ -9,7 +9,7 @@ const GOLD = '#D4AF37';
 const HISTORY_IMAGES = [
   "https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/69b57d0bb4c61271a073eceb/aa90313ee_f09ff0d6a_Screenshot2026-02-27at65008PM-1.png",
   "https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/69b57d0bb4c61271a073eceb/87bce6413_5d13e8638_Screenshot2026-02-27at65055PM.png",
-  "https://images.unsplash.com/photo-1611339555312-e607c90352fd?w=400&h=500&fit=crop"
+  "https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/69b57d0bb4c61271a073eceb/90e2b476c_768aea81c_Screenshot2026-02-27at65132PM.png"
 ];
 
 export default function Explainers() {
