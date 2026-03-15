985747e03e21ca25f7470e8b179a996af32d8cde
Robert Dyson <rdyson2622@gmail.com>
2026-03-15 20:42:25 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -109,6 +109,7 @@ import CityGuide from './pages/CityGuide';
 import Search from './pages/Search';
 import Admin from './pages/Admin';
 import AdminOwners from './pages/AdminOwners';
+import AdminOwnerDetail from './pages/AdminOwnerDetail';
 import AdminClients from './pages/AdminClients';
 import AdminListingSearch from './pages/AdminListingSearch';
 import AdminOutreachCampaigns from './pages/AdminOutreachCampaigns';
